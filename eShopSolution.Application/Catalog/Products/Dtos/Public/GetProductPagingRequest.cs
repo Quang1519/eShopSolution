269c90866ec0f4bd6838f63499f19c06095be4eb
Quang <Quang@DESKTOP-E21S7NI>
2021-07-30 22:22:47 +0700

    commit

---
+++ eShopSolution.Application/Catalog/Products/Dtos/Public/GetProductPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopSolution.Application.Dtos;
+
+namespace eShopSolution.Application.Catalog.Products.Dtos.Public
+{
+    public class GetProductPagingRequest : PagingRequestBase 
+    {
+        public int? CategoryId { get; set; }
+    }
+}
---
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Public;
+using eShopSolution.Application.Dtos;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
